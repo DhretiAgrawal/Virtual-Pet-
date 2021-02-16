fcf11a29622a2c84522f9b2083698691bd05b75a
DhretiAgrawal <68017770+DhretiAgrawal@users.noreply.github.com>
2021-02-16 22:27:17 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,45 @@
+class Food{
+    constructor(x,y){
+        this.image=loadImage("Milk.png");
+        this.lastFed; 
+        this.foodStock = 0;
+    }
+
+  display(){
+   fill(255,255,254);
+   textSize(15);
+   
+
+   var x = 80 , y = 100;
+   
+   imageMode(CENTER);
+   image(this.image,720,220,70,70);
+
+   if(this.foodStock!=0){
+       for(var i=0;i<this.foodStock;i++){
+           if(i%10==0){
+               x=80;
+               y=y+50;
+           }
+           image(this.image,x,y,50,50);
+           x=x+30;
+       }
+    }         
+  }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
+    updateFoodStock(foodStock){
+      this.foodStock=foodStock;
+    }
+
+    deductFood(){
+      if(this.foodStock>0){
+        this.foodStock=this.foodStock-1;
+      }
+    }
+
+
+}
